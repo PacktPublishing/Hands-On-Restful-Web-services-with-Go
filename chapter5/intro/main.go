f33fbac82af957f44236654b9f1d80052f4a0268
Naren Yellavula <narenarya@live.com>
2019-06-09 22:38:54 +0200

    add intro and movie API

---
+++ chapter5/intro/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// Movie holds a movie data
+type Movie struct {
+	Name      string   `bson:"name"`
+	Year      string   `bson:"year"`
+	Directors []string `bson:"directors"`
+	Writers   []string `bson:"writers"`
+	BoxOffice `bson:"boxOffice"`
+}
+
+// BoxOffice is nested in Movie
+type BoxOffice struct {
+	Budget uint64 `bson:"budget"`
+	Gross  uint64 `bson:"gross"`
+}
+
+func main() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB successfully")
+
+	collection := client.Database("appDB").Collection("movies")
+
+	// Create a movie
+	darkNight := Movie{
+		Name:      "The Dark Knight",
+		Year:      "2008",
+		Directors: []string{"Christopher Nolan"},
+		Writers:   []string{"Jonathan Nolan", "Christopher Nolan"},
+		BoxOffice: BoxOffice{
+			Budget: 185000000,
+			Gross:  533316061,
+		},
+	}
+
+	// Insert a document into MongoDB
+	result, err := collection.InsertOne(context.TODO(), darkNight)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryResult := &Movie{}
+	// bson.M is used for nested fields
+	filter := bson.M{"boxOffice.budget": bson.M{"$gt": 150000000}}
+	err = collection.FindOne(context.TODO(), filter).Decode(queryResult)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Movie:", result)
+	err = client.Disconnect(context.TODO())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("disconnected from MongoDB")
+}
