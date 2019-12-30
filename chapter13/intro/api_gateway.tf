cef333a0504cd4b9d819be34641356c91f535130
Naren Yellavula <narenarya@live.com>
2019-12-31 00:10:08 +0100

    final changes to chapter13

--- chapter13/intro/api_gateway.tf
+++ chapter13/intro/api_gateway.tf
@@ -7,7 +7,7 @@ resource "aws_api_gateway_rest_api" "test" {
   }
 }
 
-// Method is a client HTTP method
+// Method request configuration
 resource "aws_api_gateway_method" "test" {
    rest_api_id   = aws_api_gateway_rest_api.test.id
    resource_id   = aws_api_gateway_rest_api.test.root_resource_id
@@ -46,7 +46,7 @@ resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
 }
 
 // Deploy API on Gateway with test environment
-resource "aws_api_gateway_deployment" "test_deployment" {
+resource "aws_api_gateway_deployment" "test" {
    depends_on = [
      aws_api_gateway_integration.test
    ]
