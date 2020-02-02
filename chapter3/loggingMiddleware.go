7e4b8b4929d96648817e064431f89c076e4b5beb
Naren Yellavula <narenarya@live.com>
2020-02-02 13:48:53 +0100

    some refactoring

--- chapter3/loggingMiddleware.go
+++ chapter3/loggingMiddleware.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"log"
-	"os"
-	"net/http"
 )
 
-func mainLogic(w http.ResponseWriter, r *http.Request) {
+func handle(w http.ResponseWriter, r *http.Request) {
 	log.Println("Processing request!")
 	w.Write([]byte("OK"))
 	log.Println("Finished processing request")
@@ -16,7 +17,7 @@ func mainLogic(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", mainLogic)
+	r.HandleFunc("/", handle)
 	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 	http.ListenAndServe(":8000", loggedRouter)
 }
